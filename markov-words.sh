280efc38dbc3297e29fd908acf98522129fb376a
Joel Tanig <joeltanig77@gmail.com>
2021-03-02 07:36:37 -0800

    added logic to markov-words.sh

--- markov-words.sh
+++ markov-words.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
-PREVWORD="BEGIN"
-tr -c "a-zA-Z" '\n'| grep '[a-zA-Z]'| while read WORD
+PREVWORD="^"
+FIRSTWORD="^^^"
+SECONDWORD="^^"
+#Reads from the text file to only read letters and change every upper case letter
+#to lower case and read the word. Also delete any symbols that is not a letter
+tr -c "a-zA-Z" '\n' | tr [:upper:] [:lower:] | grep '[a-zA-Z]'| while read WORD
 do
-  echo "$PREVWORD $WORD"
+  #If length is 1 and we are not a,i,o, skip it
+  if [[ ${#WORD} == 1 ]] && [[ $WORD != [aio] ]]; then
+    continue
+  fi
+  #Print the new edited text
+  echo "$FIRSTWORD $SECONDWORD $PREVWORD $WORD"
+  FIRSTWORD="$SECONDWORD"
+  SECONDWORD="$PREVWORD"
   PREVWORD="$WORD"
+  continue
 done
